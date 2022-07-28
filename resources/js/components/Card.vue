4b151be47c1b2fc9d834b7d80b7243b41728ac7f
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2022-07-28 12:11:01 +0300

    Override Card component

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <Card class="nova-grid--card">
+    <slot />
+  </Card>
+</template>
+
+<script>
+import Card from 'nova/components/Card';
+
+export default {
+  components: { Card },
+};
+</script>
+
+<style lang="scss">
+form .nova-grid--card {
+  display: flex;
+  flex-wrap: wrap;
+
+  > *:last-child .field-wrapper {
+    border-style: none;
+  }
+}
+</style>
--- resources/js/entry.js
+++ resources/js/entry.js
@@ -1,7 +1,9 @@
+import Card from './components/Card';
 import PanelItem from './components/PanelItem';
 import DefaultField from './components/DefaultField';
 
 Nova.booting((Vue, router, store) => {
+  Vue.component('Card', Card);
   Vue.component('PanelItem', PanelItem);
   Vue.component('DefaultField', DefaultField);
 });
